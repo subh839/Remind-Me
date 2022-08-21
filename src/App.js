932a60e9a4434da151b7af5c3886b8cf704fb0eb
subh839 <79743346+subh839@users.noreply.github.com>
2022-08-21 13:58:01 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Home from './components/Home';
+import DataProvider from './context/DataProvider';
+
+function App() {
+  return (
+    <DataProvider>
+      <Home />
+    
+    </DataProvider>
+  );
+}
+
+export default App;
---
+++ src/components/HeaderBar.jsx
@@ -0,0 +1,39 @@
+
+import { AppBar, Toolbar, Typography, IconButton } from '@mui/material';
+import { Menu } from '@mui/icons-material';
+import { styled } from '@mui/material/styles';
+
+const Header = styled(AppBar)`
+  z-index: 1201;
+  background: #fff;
+  height: 70px;
+  box-shadow: inset 0 -1px 0 0 #dadce0;
+`
+
+const Heading = styled(Typography)`
+  color: #5F6368;
+  font-size: 24px;
+  margin-left: 25px;
+`
+
+
+const HeaderBar = ({ open, handleDrawer }) => {
+  
+  return (
+    <Header open={open}>
+      <Toolbar>
+        <IconButton
+          onClick={() => handleDrawer()}
+          sx={{ marginRight: '20px'}}
+          edge="start"
+        >
+          <Menu />
+        </IconButton>
+       
+        <Heading>Reminder</Heading>
+      </Toolbar>
+    </Header>
+  )
+}
+
+export default HeaderBar;
